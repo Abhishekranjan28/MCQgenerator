487bc73226c5318b622c66617639e906609b8ca5
Abhishekranjan28 <143013891+Abhishekranjan28@users.noreply.github.com>
2024-03-31 01:11:48 +0530

    project completed

---
+++ src/mcqgenerator/utils.py
@@ -0,0 +1,46 @@
+import os
+import PyPDF2
+import json
+import traceback
+import pandas as pd
+
+def read_file(file):
+    if file.name.endswith('.pdf'):
+        try:
+            pdf_reader=PyPDF2.PdfReader(file)
+            text=""
+            for page in pdf_reader.pages:
+                text+=page.extract_text()
+            return text
+        
+        except Exception as e:
+            raise Exception("Could not read the pdf file")
+    
+    elif file.name.endswith('.txt'):
+        return file.read().decode('utf-8')
+
+    else:
+        raise Exception("Unsupported file format")
+    
+
+import json
+import traceback
+
+def get_table_data(quiz_str):
+    try:
+        print("Quiz String:", quiz_str)  # Print the quiz string for debugging
+        quiz_dict = json.loads(quiz_str)
+        quiz_data_table = []
+
+        for key, value in quiz_dict.items():
+            mcq = value['mcq']
+            options = " || ".join([f"{option} : {option_value}" for option, option_value in value["options"].items()])
+            correct = value['correct']
+            quiz_data_table.append({'MCQ': mcq, 'options': options, 'correct': correct})
+
+        return quiz_data_table
+
+    except Exception as e:
+        traceback.print_exception(type(e), e, e.__traceback__)
+        return False
+
---
+++ streamlitapp.py
@@ -0,0 +1,83 @@
+import os
+import json
+import traceback
+import pandas as pd
+from dotenv import load_dotenv
+from src.mcqgenerator.utils import read_file,get_table_data
+
+import streamlit as st
+from langchain.callbacks import get_openai_callback
+from src.mcqgenerator.MCQGenet  import generate_evaluate_chain
+from src.mcqgenerator.logger import logging
+
+
+#read json file
+
+with open('C:\\Users\\ASUS\\Desktop\\MCQGen\\Response.json','r') as file:
+    RESPONSE_JSON=json.load(file)
+ 
+
+#creating a form using streamlit
+    
+    with st.form('user.inputs'):
+        #upload file
+
+        uploaded_file=st.file_uploader("upload a pdf or text file")
+        
+        #input fields
+        mcq_count=st.number_input("No of MCQS",min_value=3,max_value=50)
+
+        #subjects
+        subject=st.text_input("Insert subject",max_chars=20)
+
+        #Quiz tone
+        tone=st.text_input("Complexity level of questions",max_chars=20,placeholder='simple')
+
+        #Add button
+        button=st.form_submit_button("CREATE Question")
+
+        #check if button is clicked and all fields have inputs
+
+        if button and uploaded_file is not None and mcq_count and subject and tone:
+            with st.spinner("loading..."):
+                try:
+                   text=read_file(uploaded_file)
+                   #count tokens and cost of api call
+
+                   with get_openai_callback() as cb:
+                        response=generate_evaluate_chain(
+                        {
+                            'text':text,
+                            'number':mcq_count,
+                            'subject':subject,
+                            'tone':tone,
+                            'response_json':json.dumps(RESPONSE_JSON)
+                        }
+                    )
+                except Exception as e:
+                    traceback.print_exception(type(e),e,e.__traceback__)
+                    st.error("Error")
+                
+
+                else:
+                    print(f"Total Tokens:{cb.total_tokens}")
+                    print(f"Prompt Tokens:{cb.prompt_tokens}")
+                    print(f"Completion Tokens:{cb.completion_tokens}")
+                    print(f"Total Cost:{cb.total_cost}")
+
+                    if isinstance(response,dict):
+
+                        quiz=response.get('quiz')
+                        print(quiz)
+                        if quiz is not None:
+                            table_data=get_table_data(quiz)
+                            print(table_data)
+                            if table_data is not None:
+                                df=pd.DataFrame(table_data)
+                                df.index=df.index+1
+                                st.table(df)
+
+                                st.text_area(label="Review",value=response['review'])
+                            
+                            else:
+                                st.write(response)
